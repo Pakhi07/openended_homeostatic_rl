d09db46f7dc30c8e7f50f3101a06557bac05e2de
Pakhu07 <pakhibanchalia2418@gmail.com>
2025-08-21 21:11:20 +0000

    add files

--- enjoy_hcrafter_env.py
+++ enjoy_hcrafter_env.py
@@ -3,6 +3,15 @@ import sys
 from sample_factory.enjoy import enjoy
 from train_hcrafter_env import parse_args, register_custom_env_envs, register_hcrafter_models
 
+import torch
+
+# Force torch.load to always use weights_only=False
+_old_load = torch.load
+def _patched_load(*args, **kwargs):
+    kwargs["weights_only"] = False
+    return _old_load(*args, **kwargs)
+
+torch.load = _patched_load
 
 def main():
     register_custom_env_envs(enjoy=True)
--- run_hcrafter_train.bash
+++ run_hcrafter_train.bash
@@ -3,8 +3,8 @@ python train_hcrafter_env.py \
 --experiment=myhcrafter \
 --stats_avg=100 \
 --num_envs_per_worker=10 \
---with_wandb=True \
---wandb_user=ugo-nama-kun \
+--with_wandb=False \
+--wandb_user=pakhibanchalia2418 \
 --wandb_project=homeostatic_crafter \
 --wandb_group=no_maxgrad_norm \
 --gamma=0.995 \
